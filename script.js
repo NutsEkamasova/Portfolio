82aa4c48ec6a3d6e6649a6537f51c871c7c180fb
Nastya <nastya.ekamasova2014@gmail.com>
2025-10-20 13:57:35 +0300

    fix burger menu

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const menuIcon = document.querySelector('.menu-icon');
-menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('menu-icon--active');
+const menuBtn = document.querySelector('.menu-icon');
+const header = document.querySelector('.header');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('menu-icon--active');
+  header.classList.toggle('header--mobile');
 });
